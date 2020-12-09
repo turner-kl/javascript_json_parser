33da35ca4207cd1320e981608dc2ae1c552ad2fd
turner-kl <angra5296@gmail.com>
2020-12-09 14:10:43 +0900

    array

--- src/index.js
+++ src/index.js
@@ -1,67 +1,107 @@
 function jsonParser(jsonString) {
-    console.debug('start.', { jsonString });
-    this.text = jsonString;
-    this.at = 0;
-    this.currentCharacter = '';
+  console.debug('start.', { jsonString });
+  this.text = jsonString;
+  this.at = 0;
+  this.currentCharacter = '';
 
-    const next = () => {
-        this.currentCharacter = this.text.charAt(this.at);
-        this.at += 1;
-        return this.currentCharacter ? this : null;
+  const next = () => {
+    this.currentCharacter = this.text.charAt(this.at);
+    this.at += 1;
+    return this.currentCharacter ? this : null;
+  }
+
+  const getValue = () => {
+    next();
+    white();
+    console.log('value', this.currentCharacter)
+    switch (this.currentCharacter) {
+      case "{":
+        return getObject();
+      case '"':
+        return getString();
+      case "[":
+        return getArray();
+      default:
+        return getNumber();
     }
+  };
 
-    const getValue = () => {
+  const getObject = () => {
+    const object = {};
+    next();
+    let key = getString();
+    let value = getValue();
+    object[key] = value;
+    return object;
+  }
+
+  /**
+   * 文字列を解析する。
+   */
+  const getString = () => {
+    let string = '';
+    while (next()) {
+      if (this.currentCharacter === '"') {
         next();
-        white();
-        switch (this.currentCharacter) {
-            case "{":
-                return getObject();
-            case '"':
-                return getString();
-        }
-    };
+        break;
+      };
+      string += this.currentCharacter;
+    }
+    return string;
+  }
 
-    const getObject = () => {
-        const object = {};
-        next();
-        let key = getString();
-        let value = getValue();
-        object[key] = value;
-        return object;
+  const getNumber = () => {
+    let string = '';
+    while (this.currentCharacter >= "0" && this.currentCharacter <= "9") {
+      string += this.currentCharacter;
+      next();
     }
 
-    /**
-     * 文字列を解析する。
-     */
-    const getString = () => {
-        let string = '';
-        while (next()) {
-            if (this.currentCharacter === '"') {
-                next();
-                break;
-            };
-            string += this.currentCharacter;
-        }
-        return string;
+    if (this.currentCharacter === ".") {
+      string += this.currentCharacter;
+      while (next() && this.currentCharacter >= "0" && this.currentCharacter <= "9") {
+        string += this.currentCharacter;
+
+      }
     }
+    const number = parseFloat(string);
 
-    /*
-     * ホワイトスペースを無視する関数
-     */
-    const white = () => {
-        while (this.currentCharacter && this.currentCharacter <= " ") {
-            next();
-        }
-    };
+    return isNaN(number) ? new Error('not a number') : number;
+  }
 
-    // ---------
-    // value
-    return (() => {
-        return getValue();
-    })();
+  const getArray = () => {
+    const array = [];
+    while (next()) {
+      white();
+      let element
+      if (this.currentCharacter === '"') {
+        element = getString();
+      } else if (this.currentCharacter >= "0" && this.currentCharacter <= "9") {
+        element = getNumber();
+      }
+      array.push(element);
+      if (this.currentCharacter !== ",") break;
+    }
+    return array;
+  }
+
+  /*
+   * ホワイトスペースを無視する関数
+   */
+  const white = () => {
+    while (this.currentCharacter && this.currentCharacter <= " ") {
+      next();
+    }
+  };
+
+  // ---------
+  // value
+  return (() => {
+    return getValue();
+  })();
 
 }
 
 module.exports = {
-    jsonParser
+  jsonParser
 }
--- src/index.spec.js
+++ src/index.spec.js
@@ -4,15 +4,62 @@ const { assert } = require("chai");
 const { jsonParser } = require("./index");
 
 describe("json_parse", () => {
-  it("単純なKeyValue", () => {
-    const string1 = '{"first": "Jerome"}';
-    const expect1 = JSON.parse(string1);
-    assert.deepEqual(jsonParser(string1), expect1);
-  });
+  describe("階層", () => {
+    it("1層", () => {
+      const string = '{"first": "Jerome"}';
+      assert.deepEqual(jsonParser(string), JSON.parse(string));
+    });
 
-  it("階層構造になったKeyValue", () => {
-    const string2 = '{"first": {"aaa":"bbb"}}';
-    const expect2 = JSON.parse(string2);
-    assert.deepEqual(jsonParser(string2), expect2);
+    it("2層", () => {
+      const string = '{"first": {"aaa":"bbb"}}';
+      assert.deepEqual(jsonParser(string), JSON.parse(string));
+    });
+
+    it("3層", () => {
+      const string = '{"first": {"aaa":{"bbb":"ccc"}}}';
+      assert.deepEqual(jsonParser(string), JSON.parse(string));
+    });
+  })
+
+  describe("各種データ型", () => {
+    it("文字列", () => {
+      const string = '{"first": "Jerome"}';
+      assert.deepEqual(jsonParser(string), JSON.parse(string));
+    });
+
+    describe("数値", () => {
+      it("整数1桁", () => {
+        const string = '{"first": 1}';
+        assert.deepEqual(jsonParser(string), JSON.parse(string));
+      });
+
+      it("整数2桁", () => {
+        const string = '{"first": 10}';
+        assert.deepEqual(jsonParser(string), JSON.parse(string));
+      });
+
+      it("小数", () => {
+        const string = '{"first": 1.5}';
+        assert.deepEqual(jsonParser(string), JSON.parse(string));
+      });
+    })
+
+    describe("配列", () => {
+      it("文字列配列", () => {
+        const string = '{"first": ["aaa", "bbb", "ccc"]}';
+        assert.deepEqual(jsonParser(string), JSON.parse(string));
+      });
+
+      it("数値配列", () => {
+        const string = '{"first": [1, 2, 3]}';
+        assert.deepEqual(jsonParser(string), JSON.parse(string));
+      });
+
+      it("数値と文字列配列", () => {
+        const string = '{"first": [1, 2, 3, "aaa"]}';
+        assert.deepEqual(jsonParser(string), JSON.parse(string));
+      });
+    });
+
   });
 });
